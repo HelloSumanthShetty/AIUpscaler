baa4473306c4bd2d59cfaa4e2135a43debe0dfb6
sumanth u shetty <sumanthshetty883@gmail.com>
2025-12-07 13:56:53 +0530

    production

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-
+import { CONFIG } from '../config';
 const AuthContext = createContext();
 
 const USAGE_STORAGE_KEY = 'daily_usage_count';
