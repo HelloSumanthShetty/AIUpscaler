aa03eb3a47cc5310a34ac1b2488859cf2738307d
sumanth u shetty <sumanthshetty883@gmail.com>
2025-12-07 13:29:58 +0530

    production

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["https://aiupscaler-1.onrender.com"]
+    host: "0.0.0.0",
+    allowedHosts: ["aiupscaler-1.onrender.com"]
   }
 })
